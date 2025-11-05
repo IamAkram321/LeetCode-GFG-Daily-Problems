63ad6eb64f2ca6237b5208bb40025623166a5a0e
MD Akram <115627463+IamAkram321@users.noreply.github.com>
2025-11-05 08:51:37 +0000

    feat(leetcode): add solution for (#122)

---
+++ lc122.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public: //T.C: O(n) and S.C: O(1)
+    int maxProfit(vector<int>& prices) {
+        int total_profit = 0;
+
+        for(int i=0;i<prices.size()-1;i++){
+            if(prices[i+1] > prices[i]){
+                total_profit += prices[i+1] - prices[i];
+            }
+        }
+        return total_profit;
+    }
+};
