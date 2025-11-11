1f952557eb5f1325ece2f1a51ebf02995acdcab8
MD Akram <115627463+IamAkram321@users.noreply.github.com>
2025-11-11 10:08:57 +0000

    feat(leetcode): add solution for (#2169)

---
+++ lc2169.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public: // T.C: O(max(num1, num2)) and S.C : O(1)
+    int countOperations(int num1, int num2) {
+        int operations = 0;
+        while(num1>0 && num2 > 0){
+            if(num1 >= num2){
+                num1 = num1 - num2;
+            }else{
+                num2 = num2 - num1;
+            }
+            operations++;
+        }
+        return operations;
+    }
+};
+
+********more optimized*********************
+
+class Solution {
+public: // T.C: O(log(max(num1, num2))) and S.C : O(1)
+    int countOperations(int num1, int num2) {
+        int operations = 0;
+        while(num1>0 && num2 > 0){
+            if(num1 >= num2){
+                operations += num1/num2;
+                num1 %= num2;
+            }else{
+                operations += num2/num1;
+                num2 %= num1;
+            }
+        }
+        return operations;
+    }
+};
