660431694e2b678ced21183a30269549f64b9160
MD Akram <115627463+IamAkram321@users.noreply.github.com>
2025-11-17 16:26:29 +0000

    feat(gfg): add solution for day (#368)

---
+++ gfg_potd_day_368.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public: //T.C: O(N*N) and S.C: O(N)
+    int maxSumIS(vector<int>& arr) {
+        
+        int n = arr.size();
+        vector<int> dp(n);
+        
+        for(int i=0;i<n;i++){
+            dp[i] = arr[i];
+        }
+        
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(arr[j]<arr[i]){
+                    dp[i] = max(dp[i],dp[j]+arr[i]);
+                }
+            }
+        }
+        return *max_element(dp.begin(),dp.end());
+    }
+};
