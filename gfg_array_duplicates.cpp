a5a962bf8701b4797f8da8df4bd08ff13e99210d
MD Akram <115627463+IamAkram321@users.noreply.github.com>
2025-12-09 18:31:38 +0000

    add solution for gfg-remove-array-duplicates-potd

---
+++ gfg_array_duplicates.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public: //T.C: O(n) and S.C: O(1)
+    vector<int> findDuplicates(vector<int>& arr) {
+        vector<int> ans;
+        int n = arr.size();
+
+        for(int i = 0; i < n; i++){
+            int index = abs(arr[i]) - 1;
+            if(arr[index] < 0)
+                ans.push_back(abs(arr[i]));
+            else
+                arr[index] = -arr[index];
+        }
+        return ans;
+    }
+};
