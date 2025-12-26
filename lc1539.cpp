c6fc9e62777442108686507f87ae149801d31f30
MD Akram <115627463+IamAkram321@users.noreply.github.com>
2025-12-26 18:21:39 +0000

    add solution for lc1539

---
+++ lc1539.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public: //T.C: O(n) and S.C: O(1)
+    int findKthPositive(vector<int>& arr, int k) {
+        // for(int i=0;i<arr.size();i++){
+        //     if(arr[i] <= k){
+        //         k++;
+        //     }else{
+        //         break;
+        //     }
+        // }
+        // return k;
+        
+
+        //T.C: O(log n) and S.C: O(1)
+        int low = 0, high = arr.size()-1;
+
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            int missing = arr[mid]-(mid+1);
+
+            if(missing < k){
+                low = mid+1;
+            }else{
+                high =  mid-1;
+            }
+        }
+        return low + k;
+    }
+};
