3b7c77ff92266928bcd38de95b6792bdc472f735
MD Akram <115627463+IamAkram321@users.noreply.github.com>
2026-01-14 16:13:00 +0000

    add solution for lc771 jewels and stone

---
+++ jewels_and_stones.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public: //T.C: O(n) and S.C: O(n)
+    int numJewelsInStones(string jewels, string stones) {
+        unordered_set<char> st(jewels.begin(), jewels.end());
+
+        int count = 0;
+        for(char c : stones){
+            if(st.count(c)) count++;
+        }
+        return count;
+    }
+};
