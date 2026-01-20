0e87283fd1cc64d9f4dce9ab463fbe5b5bb45645
MD Akram <115627463+IamAkram321@users.noreply.github.com>
2026-01-20 18:00:40 +0000

    undo and redo from gfg_potd

---
+++ undo_and_redo.cpp
@@ -0,0 +1,37 @@
+class Solution {
+  private:
+    string doc;
+    stack<char> undoStack;
+    stack<char> redoStack;
+
+  public:
+    void append(char x) {
+        doc.push_back(x);
+        undoStack.push(x);
+        while (!redoStack.empty()) redoStack.pop();
+    }
+
+    void undo() {
+        if (undoStack.empty()) return;
+
+        char last = undoStack.top();
+        undoStack.pop();
+
+        doc.pop_back();
+        redoStack.push(last);
+    }
+
+    void redo() {
+        if (redoStack.empty()) return;
+
+        char ch = redoStack.top();
+        redoStack.pop();
+
+        doc.push_back(ch);
+        undoStack.push(ch);
+    }
+
+    string read() {
+        return doc;
+    }
+};
