9e1d015988f58ad0c2de041511eedd1a0b52e556
MD Akram <115627463+IamAkram321@users.noreply.github.com>
2026-01-28 16:18:53 +0000

    add multiple optimized  solution for the problem

--- count_subset_with_target_sum_ii.cpp
+++ count_subset_with_target_sum_ii.cpp
@@ -35,3 +35,53 @@ class Solution {
         return countSubsets(arr, mid, n, leftSums, k, 0);
     }
 };
+ /*****************************************************************************************/
+  class Solution {
+  public: //T.C: O(2^n) and S.C: O(n)
+  
+    int recurSubset(int index, int sum, vector<int>& arr, int k){
+        
+        if(index==arr.size()){
+           return sum == k;
+        }
+        int take    = recurSubset(index+1,sum+arr[index],arr,k);
+        int notTake = recurSubset(index+1,sum,arr,k);
+        return take+notTake;
+    }
+  
+    int countSubset(vector<int> &arr, int k) {
+        return recurSubset(0,0,arr,k);
+    }
+};
+
+/**************************************************************************************************** */
+
+class Solution {
+  public: // T.C: O(n*k) and S.C: O(n*k)
+  
+    int dp[1001][1001]; 
+  
+    int recurSubset(int index, int sum, vector<int>& arr, int k){
+        
+        if(index == arr.size()){
+           return sum == k;
+        }
+        if(dp[index][sum] != -1){
+            return dp[index][sum];
+        }
+        
+        int take = 0, notTake = 0;
+        if(sum + arr[index] <= k){
+            take = recurSubset(index+1, sum+arr[index], arr, k);
+        }
+        
+        notTake = recurSubset(index+1, sum, arr, k);
+        
+        return dp[index][sum] = take + notTake;
+    }
+  
+    int countSubset(vector<int> &arr, int k) {
+        memset(dp, -1, sizeof(dp));
+        return recurSubset(0, 0, arr, k);
+    }
+};
