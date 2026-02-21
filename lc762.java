96fafdc6b7672e4c9a2f7ac75ec0ed27f2019197
MD Akram <115627463+IamAkram321@users.noreply.github.com>
2026-02-21 17:44:50 +0000

    add solution for lc762

---
+++ lc762.java
@@ -0,0 +1,22 @@
+class Solution {
+    //T.C: O(N) and S.C: O(1)
+    private boolean isPrime(int n){
+        if(n<=1) return false;
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public int countPrimeSetBits(int left, int right) {
+        int count = 0;
+        for(int i=left;i<=right;i++){
+            int setBits=Integer.bitCount(i);
+            if(isPrime(setBits)){
+                count++;
+            }
+        }
+        return count;
+    }
+}
