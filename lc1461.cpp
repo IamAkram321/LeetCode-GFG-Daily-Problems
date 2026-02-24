4431f5a2fdf48771f7117cb13f5f1892bf93d5f6
MD Akram <115627463+IamAkram321@users.noreply.github.com>
2026-02-24 10:43:48 +0000

    add solution for lc1461

---
+++ lc1461.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public: //T.C: O(n*k) and S.C: O(2^k)
+    bool hasAllCodes(string s, int k) {
+       int n = s.size();
+        unordered_set<string> st;
+        for(int i = 0; i <= n - k; i++) {
+            st.insert(s.substr(i, k));
+        }
+        return st.size() == (1 << k);
+    }
+};
