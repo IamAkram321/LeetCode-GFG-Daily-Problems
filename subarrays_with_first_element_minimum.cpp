f5cc2f6ed23d8378b602ddb3e23225d35e6dd64a
MD Akram <115627463+IamAkram321@users.noreply.github.com>
2026-03-10 18:22:08 +0000

    add solution for subarrays with fist element minimum from gfg_potd

---
+++ subarrays_with_first_element_minimum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    int countSubarrays(vector<int> &arr) {
+        int n = arr.size();
+        stack<int> st;
+        vector<int> nextSmaller(n, n);
+        for(int i = n-1; i >= 0; i--){
+            while(!st.empty() && arr[st.top()] >= arr[i]){
+                st.pop();
+            }
+            if(!st.empty()){
+                nextSmaller[i] = st.top();
+            }
+            st.push(i);
+        }
+        int count = 0;
+        for(int i = 0; i < n; i++){
+            count += nextSmaller[i] - i;
+        }
+        return count;
+    }
+};
