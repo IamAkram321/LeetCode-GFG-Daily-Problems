83a4a5b5fc51bb23113371a7d65c033aa07af932
MD Akram <115627463+IamAkram321@users.noreply.github.com>
2026-04-04 08:23:49 +0000

    add solution for gray_code

---
+++ gray_code.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<string> graycode(int n) {
+        vector<string> result;
+
+        int total = 1 << n; // 2^n
+
+        for (int i = 0; i < total; i++) {
+            int gray = i ^ (i >> 1);
+            string s = "";
+            for (int j = n - 1; j >= 0; j--) {
+                if (gray & (1 << j)) s += '1';
+                else s += '0';
+            }
+
+            result.push_back(s);
+        }
+
+        return result;
+    }
+};
