c6cbf7fa81e05e99508c111c5dfbd25ecc7e4f79
MD Akram <115627463+IamAkram321@users.noreply.github.com>
2026-04-17 11:29:59 +0000

    add solution for anagram palindrome

---
+++ anagram_palindrome.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public: //T.C: O(n) and S.C:O(1)
+    bool canFormPalindrome(string &s) {
+        vector<int>freq(26,0);
+        for(char ch : s){
+            freq[ch-'a']++;
+        }
+        int oddCount=0;
+        for(int i=0;i<26;i++){
+            if(freq[i]%2!=0){
+                oddCount++;
+            }
+        }
+        return oddCount<=1;
+    }
+};
