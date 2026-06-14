273529452d634de218ff66b503f4687ddecf11b4
MD Akram <115627463+IamAkram321@users.noreply.github.com>
2026-06-14 09:00:46 +0000

    add solution for exit point in a matrix

---
+++ exit_point_in_a_matrix.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> exitPoint(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        int i = 0, j = 0;
+        int dir = 0; 
+        while (i >= 0 && i < n && j >= 0 && j < m) {
+            if (mat[i][j] == 1) {
+                dir = (dir + 1) % 4;
+                mat[i][j] = 0;
+            }
+            if (dir == 0) j++;      
+            else if (dir == 1) i++; 
+            else if (dir == 2) j--; 
+            else i--;               
+        }
+        if (dir == 0) j--;
+        else if (dir == 1) i--;
+        else if (dir == 2) j++;
+        else i++;
+        return {i, j};
+    }
+};
