d4456a75e3de5749da93b0f30ca859145ca8a054
MD Akram <115627463+IamAkram321@users.noreply.github.com>
2026-06-16 17:15:44 +0000

    add solution for construct list using xor queries from gfg potd

---
+++ construct_list_using_xor_queries.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> constructList(vector<vector<int>> &queries) {
+
+        vector<int> arr;
+        arr.push_back(0);   
+        int xr = 0;
+        for (auto &q : queries) {
+            int type = q[0];
+            int x = q[1];
+            if (type == 0) {
+                arr.push_back(x ^ xr);
+            } else {
+                xr ^= x;
+            }
+        }
+        for (int &v : arr) {
+            v ^= xr;
+        }
+        sort(arr.begin(), arr.end());
+        return arr;
+    }
+};
